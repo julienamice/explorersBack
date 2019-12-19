807f096c7b216fd1760a2fe2ef0583ee4e154fef
Eva Aré-Hermas <eva.ahermas@gmail.com>
2019-12-19 15:10:18 +0100

    push Eva

--- routes/users.js
+++ routes/users.js
@@ -1,110 +1,101 @@
 var express = require("express");
-var router = express.Router();
 var bdd = require("../models/bdd");
-var userModel = require("../models/users.model");
+const fs = require('fs');
+const path = require('path');
+var router = express.Router();
+var userModel = require('../models/users.model')
+var uid2 = require('uid2')
+var SHA256 = require('crypto-js/sha256');
+var encBase64 = require("crypto-js/enc-base64");
+
 
 /* GET home page. */
-router.get("/:id", function(req, res, next) {
-  var userInfos = [
-    {
-      id: 1,
-      name: "Martin",
-      lastname: "Dupont",
-      email: "mdupont@gmail.com",
-      teacher: true
-    },
-    {
-      id: 2,
-      name: "Vanessa",
-      lastname: "Smith",
-      email: "vaness@gmail.com",
-      teacher: false
-    },
-    {
-      id: 3,
-      name: "Jon",
-      lastname: "Doe",
-      email: "jondoe@gmail.com",
-      teacher: true
-    }
-  ];
+router.get('/signup', async function (req, res, next) {
 
-  const user = userInfos.find(user => user.id === parseInt(req.params.id));
-  console.log(user);
-  res.json(user);
+  let emailFF = req.query.email
+  let passwordFF = req.query.password
+  let isLogged = false
+  let checkUserDB = await userModel.findOne({
+    email: emailFF
+  })
+
+  if (req.query.email == "" || req.query.password == "") {
+    console.log('User doesnt signup')
+    res.render('login');
+  }
+
+  if (checkUserDB) {
+    // console.log('UserExist')
+    // let hash = SHA256(passwordFF + checkUserDB.salt).toString(encBase64)
+
+    // if (hash === checkUserDB.password) {
+    //   let newToken = uid2(32)
+    //   let updateToken = await userModel.updateOne({ email: emailFF }, { token: newToken })
+    //   isLogged = true
+    //   console.log(isLogged + '   ' + newToken)
+    //   res.json({ isLogged, userToken: newToken, id: checkUserDB.id, user: checkUserDB });
+    // } else {
+
+    //   isLogged = false
+    //   res.json({ isLogged, userToken: 'Pas Login' });
+    // }
+
+    res.json({ result: false, errorMsg: 'already exist' });
+
+  } else {
+    console.log('UserNoExist')
+    let salt = uid2(32)
+    let newToken = uid2(32)
+    let newUser = new userModel({
+      email: emailFF,
+      password: SHA256(passwordFF + salt).toString(encBase64),
+      token: newToken,
+      salt: salt
+    })
+
+    let userSave = await newUser.save()
+    isLogged = true
+    console.log(userSave)
+    res.json({ result: true, userSave });
+
+  }
+
 });
 
-// POST inscription
-router.post("/signup", async function(req, res, next) {
-  console.log("on est dans signup", req.body);
-  var newUser = new userModel({
-    firstName: req.body.firstName,
-    email: req.body.email,
-    password: req.body.password,
-    isTeacher: req.body.isTeacher
-  });
 
-  newUser.save(function(error, user) {
-    if (error) {
-      console.log("Oups...error ->", error);
+router.get('/signin', async function (req, res, next) {
+
+  let emailFF = req.query.email
+  let passwordFF = req.query.password
+  let isLogged = false
+  let checkUserDB = await userModel.findOne({
+    email: emailFF
+  })
+
+  console.log(checkUserDB);
+  if (req.query.email == "" || req.query.password == "") {
+    console.log('User doesnt signup')
+    isLogged = false;
+    res.json({ isLogged });
+  }
+
+  if (checkUserDB) {
+    let hash = SHA256(passwordFF + checkUserDB.salt).toString(encBase64)
+    if (hash === checkUserDB.password) {
+      res.json({ result: true, user: checkUserDB });
     } else {
-      console.log("user ->", user);
-      console.log("body", req.body);
-
-      res.json(user);
+      res.json({ result: false, errorMsg: 'email or password not valid' });
     }
-  });
 
-  // // We are checking it there is a user or not
-  // if (user) {
-  //   console.log("We found a User with this email", user);
-  //   res.json({ user });
-  // } else {
-  //   console.log(
-  //     "There is no user with this email ! So we need to add the user"
-  //   );
-  //   var newUser = new userModel({
-  //     first_name: req.body.firstName,
-  //     email: req.body.email
-  //   });
+  } else {
+    res.json({ result: false, errorMsg: 'email not exist' });
+  }
 
-  //   newUser.save(function(error, user) {
-  //     if (error) {
-  //       console.log("Oups...error ->", error);
-  //     } else {
-  //       console.log("Here is our new Liked user ->", user);
-  //       console.log("user", req.body);
-
-  //       res.json({ result: true, user });
-  //     }
-  //   });
-  // }
 });
 
-// POST connexion
-router.post("/signin", function(req, res) {
-  //  if (req.body.firstName=== && req.body.password ===) {
-
-  userModel.findOne(
-    { firstName: req.body.firstName, password: req.body.password },
-    function(err, user) {
-      if (user) {
-        console.log("dans ma base de donnée --->", user);
-
-        // user.save();
-        res.json(user);
-      } else {
-        console.log(err);
-        res.json({ result: false });
-      }
-    }
-  );
-});
-
-// DELETE suppression
-router.delete("/:_id", async function(req, res, next) {
+router.delete("/:_id", async function (req, res, next) {
   console.log("CA DEVRAIT PASSER LA !!!", req.params);
-  userModel.deleteOne({ _id: req.params._id }, function(err, user) {
+  userModel.deleteOne({ _id: req.params._id }, function (err, user) {
     if (err) {
       console.log("Oups...error ->", err);
       res.json({ result: false });
@@ -116,8 +107,5 @@ router.delete("/:_id", async function(req, res, next) {
     console.log("user", req.params._id);
   });
 });
-
-// (POST edit )
-// (POST menu )
 
 module.exports = router;
